3099ce9ee66d3a67823ecbcc7840043e3c0fb284
Renan-Silva235 <renan.rubio95@gmail.com>
2025-07-11 21:10:09 -0300

    boolean structures to access certain data

---
+++ include/structs.h
@@ -0,0 +1,12 @@
+#ifndef STRUCTS_H
+#define STRUCTS_H
+
+#include <stdbool.h>
+
+typedef struct {
+    bool updateDescription;
+    bool updatePrice;
+    bool updateAmount;
+} UpdateOptions;
+
+#endif
