43915e987581abad846334de4183069d9f45e0a9
Renan-Silva235 <renan.rubio95@gmail.com>
2025-07-11 21:08:12 -0300

    functions responsible for database manipulation

---
+++ include/database.h
@@ -0,0 +1,9 @@
+#include <mysql/mysql.h>
+#include <stdbool.h>
+#include "structs.h"
+
+void insertProduct(MYSQL *connect, char *product, char *descriptionProduct, float *price, int *amount);
+void listProduct(MYSQL *connect);
+void searchProduct(MYSQL *connect, char *product);
+void updateProduct(MYSQL *connect, UpdateOptions opt, char *data);
+void deleteProduct(MYSQL *connect, char *nameProduct);
