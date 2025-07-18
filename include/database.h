2ed8b0695b8a606958b7a2cf62a0ba4245f5456e
Renan Silva <renan.rubio94@gmail.com>
2025-07-18 11:29:14 -0300

    Delete include/database.h

--- include/database.h
+++
@@ -1,9 +0,0 @@
-#include <mysql/mysql.h>
-#include <stdbool.h>
-#include "structs.h"
-
-void insertProduct(MYSQL *connect, char *product, char *descriptionProduct, float *price, int *amount);
-void listProduct(MYSQL *connect);
-void searchProduct(MYSQL *connect, char *product);
-void updateProduct(MYSQL *connect, UpdateOptions opt, char *data);
-void deleteProduct(MYSQL *connect, char *nameProduct);
